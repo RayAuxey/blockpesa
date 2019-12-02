e4ccba9d50c50de236146981639fb07223ea2fc8
Ray Auxey <rayauxey@gmail.com>
2019-12-02 17:30:47 +0300

    init2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require("express"),
 const PORT = config.get("app.port");
 const db = config.get("db.name");
 
-mongoose.connect(`mongodb://localhost:27017/${db}`, { useNewUrlParser: true });
+mongoose.connect(`mongodb://rayauxey:!1Gaishah@mflix-shard-00-00-mk7jy.mongodb.net:27017,mflix-shard-00-01-mk7jy.mongodb.net:27017,mflix-shard-00-02-mk7jy.mongodb.net:27017/BlockPesa?ssl=true&replicaSet=mflix-shard-0&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true });
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
