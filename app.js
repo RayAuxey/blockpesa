d2c23c72cab277fb32c74f0be39d79fbbc58fae7
Ray Auxey <rayauxey@gmail.com>
2019-12-03 08:41:24 +0300

    init4

--- app.js
+++ app.js
@@ -23,7 +23,16 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+var whitelist = ['http://localhost:8080']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 
 app.use(cors());
