ba620bf2252600e1e226ce76919cae5ae63b0c4d
Ray Auxey <rayauxey@gmail.com>
2019-12-03 08:05:14 +0300

    init4

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express"),
   app = express();
 
 var http = require('http').createServer(app);
-var io = require('socket.io')(http, { origins: '*:*'});
+var io = require('socket.io')(http, { origins: 'http://localhost:8080'});
 
 const PORT = process.env.PORT || 8001;
 const db = config.get("db.name");
